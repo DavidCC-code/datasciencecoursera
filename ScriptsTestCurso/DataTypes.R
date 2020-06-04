7545dae535f07fb08bc50c168866c0667f6b8645
DavidCC-code <contrerasdavid0@hotmail.com>
2020-06-04 16:40:43 +0200

    Added last part about NAMES

--- ScriptsTestCurso/DataTypes.R
+++ ScriptsTestCurso/DataTypes.R
@@ -179,3 +179,37 @@ nrow(x)
 # [1] 4
 ncol(x)
 # [1] 2
+
+
+## NAMES
+
+x <- 1:3
+names(x)
+# NULL
+
+names(x) <- c("foo","bar", "norf")
+names(x)
+# [1] "foo"  "bar"  "norf"
+
+x
+# foo  bar norf 
+# 1    2    3 
+
+x<-list(a=1,b=2,c=3)
+x
+# $a
+# [1] 1
+# 
+# $b
+# [1] 2
+# 
+# $c
+# [1] 3
+m <- matrix(1:4, nrow=2, ncol=2)
+dimnames(m) <- list(c("a","b"),c("c","d"))
+m
+# c d
+# a 1 3
+# b 2 4
+
+
